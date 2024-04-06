1f3a72ffa923032c71543b8748932275a8975c14
Sushanta Shrestha <142810781+sshres10@users.noreply.github.com>
2024-04-06 09:13:57 -0400

    app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,3 +1,7 @@
+# --------------------------------------------
+# Imports at the top - PyShiny EXPRESS VERSION
+# --------------------------------------------
+
 # From shiny, import just reactive and render
 from shiny import reactive, render
 
@@ -8,52 +12,150 @@ from shiny.express import ui
 import random
 from datetime import datetime
 
-# Import icons
+# --------------------------------------------
+# Import icons as you like
+# --------------------------------------------
+
 from faicons import icon_svg
 
-# Set up the reactive content
+# --------------------------------------------
+# FOR LOCAL DEVELOPMENT
+# --------------------------------------------
+# Add all packages not in the Std Library
+# to requirements.txt:
+#
+# faicons
+# shiny
+# shinylive
+# 
+# And install them into an active project virtual environment (usually in .venv)
+# --------------------------------------------
 
-UPDATE_INTERVAL_SECS: int = 1  # Update interval in seconds
+# --------------------------------------------
+# SET UP THE REACIVE CONTENT
+# --------------------------------------------
+
+# --------------------------------------------
+# PLANNING: We want to get a fake temperature and 
+# Time stamp every N seconds. 
+# For now, we'll avoid storage and just 
+# Try to get the fake live data working and sketch our app. 
+# We can do all that with one reactive calc.
+# Use constants for update interval so it's easy to modify.
+# ---------------------------------------------------------
+
+# --------------------------------------------
+# First, set a constant UPDATE INTERVAL for all live data
+# Constants are usually defined in uppercase letters
+# Use a type hint to make it clear that it's an integer (: int)
+# --------------------------------------------
+
+UPDATE_INTERVAL_SECS: int = 1
+
+# --------------------------------------------
+# Initialize a REACTIVE CALC that our display components can call
+# to get the latest data and display it.
+# The calculation is invalidated every UPDATE_INTERVAL_SECS
+# to trigger updates.
+# It returns everything needed to display the data.
+# Very easy to expand or modify.
+# (I originally looked at REACTIVE POLL, but this seems to work better.)
+# --------------------------------------------
+
 
 @reactive.calc()
 def reactive_calc_combined():
     # Invalidate this calculation every UPDATE_INTERVAL_SECS to trigger updates
     reactive.invalidate_later(UPDATE_INTERVAL_SECS)
 
-    # Data generation logic
-    temp = round(random.uniform(-18, -16), 1)  # Generate a random temperature
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Get current timestamp
+    # Data generation logic. Get random between -18 and -16 C, rounded to 1 decimal place
+    temp = round(random.uniform(-18, -16), 1)
 
-    return {"temp": temp, "timestamp": timestamp}
+    # Get a timestamp for "now" and use string format strftime() method to format it
+    # 
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    latest_dictionary_entry = {"temp": temp, "timestamp": timestamp}
 
-# Define the UI layout - Page Options
+    # Return everything we need
+    return latest_dictionary_entry
+
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Page Options
+# ------------------------------------------------
+
+# Call the ui.page_opts() function
+# Set title to a string in quotes that will appear at the top
+# Set fillable to True to use the whole page width for the UI
 
 ui.page_opts(title="PyShiny Express: Live Data (Basic)", fillable=True)
 
-# Define the UI layout - Sidebar
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Sidebar
+# ------------------------------------------------
+
+# Sidebar is typically used for user interaction/information
+# Note the with statement to create the sidebar followed by a colon
+# Everything in the sidebar is indented consistently
 
 with ui.sidebar(open="open"):
+
     ui.h2("Antarctic Explorer", class_="text-center")
-    ui.p("A demonstration of real-time temperature readings in Antarctica.", class_="text-center")
 
-# Define the UI layout - Main Section
+    ui.p(
+        "A demonstration of real-time temperature readings in Antarctica.",
+        class_="text-center",
+    )
 
+    ui.hr()
+
+    ui.h6("Links:")
+
+    ui.a(
+        "GitHub Source",
+        href="https://github.com/denisecase/cintel-05-cintel-basic",
+        target="_blank",
+    )
+
+    ui.a(
+        "GitHub App",
+        href="https://denisecase.github.io/cintel-05-cintel-basic/",
+        target="_blank",
+    )
+
+    ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
+
+
+#---------------------------------------------------------------------
+# In Shiny Express, everything not in the sidebar is in the main panel
+#---------------------------------------------------------------------
+    
 ui.h2("Current Temperature")
 
 @render.text
 def display_temp():
-    """Get the latest reading and return a temperature string."""
+    """Get the latest reading and return a temperature string"""
     latest_dictionary_entry = reactive_calc_combined()
     return f"{latest_dictionary_entry['temp']} C"
 
 ui.p("warmer than usual")
 icon_svg("sun")
+
+
 ui.hr()
 
 ui.h2("Current Date and Time")
 
 @render.text
 def display_time():
-    """Get the latest reading and return a timestamp string."""
+    """Get the latest reading and return a timestamp string"""
     latest_dictionary_entry = reactive_calc_combined()
     return f"{latest_dictionary_entry['timestamp']}"
+
+
+with ui.layout_columns():
+    with ui.card():
+        ui.card_header("Current Data (placeholder only)")
+
+with ui.layout_columns():
+    with ui.card():
+        ui.card_header("Current Chart (placeholder only)")
